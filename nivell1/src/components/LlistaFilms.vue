14b85ef1c621f59226fabb1d26c5c42af34e40bf
Jordi Vidaña <jordivid@gmail.com>
2021-03-08 19:42:31 +0100

    Càrrega inicial

---
+++ nivell1/src/components/LlistaFilms.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <!-- <Bounce> -->
+
+    <div class="container mt-2 pb-2 d-flex flex-wrap justify-content-between align-content-start" id="llista" v-if="show">
+        <div class="alert alert-danger w-100 mt-3" v-if="SelectedMovies.length == 0">No hi ha películes disponibles</div>
+        <div class="card mt-2" v-for="SelectedMovie of SelectedMovies" :key="SelectedMovie.id">
+            <div class="card-header">
+                <h5>{{SelectedMovie.title}}</h5>
+                <span>Available: {{SelectedMovie.available}}</span>
+            </div>
+            <div class="card-body d-flex align-items-center">{{SelectedMovie.description}}</div>
+        </div>
+    </div>
+
+    <!-- </Bounce> -->
+
+    </div>
+</template>
+
+<script src="./../scripts/LlistaFilms.js"></script>
+
+<style src="./../styles/LlistaFilms.css" scoped></style>
---
+++ nivell1/src/components/animations/Bounce.vue
@@ -0,0 +1,9 @@
+<template>
+    <transition
+        enter-active-class="animated__bounceIn">
+        <slot></slot>
+    </transition>
+</template>
+
+<script src="./../../scripts/animations/Bounce.js"></script>
+<style src="./../../styles/animations/Bounce.css" scoped></style>
